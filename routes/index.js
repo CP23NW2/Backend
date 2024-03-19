398159e370eae369b1c83a0a7d8400d7c8affc06
pprrreaww <pprrreaww@gmail.com>
2024-03-19 15:08:17 +0700

    route validate

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const eyewear = require('./eyewears')
 const admin = require('./admin')
 const customer= require('./customers')
 const order = require('./order')
+const validate = require('./validate')
 
 router.use(admin);
 router.use(eyewear);                           
 router.use(customer);
 router.use(order);
+router.use(validate)
 
 module.exports = router;
